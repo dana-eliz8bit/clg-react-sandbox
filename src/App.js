527c27f3da84fe5f91c6499a0e775922cbad6cce
dana.eliz8bit <94809868+dana-eliz8bit@users.noreply.github.com>
2022-11-17 11:01:03 +1000

    Updated App.js arrow function.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import List from "./components/List";
 
-export default function App() {
+const App = () => {
   return (
     <div className="App">
       <h1>My Food Wish List</h1>
       <List />
     </div>
   );
-}
+};
+
+export default App;
